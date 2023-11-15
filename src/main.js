43723051384221e8bf885880782e9443872110f9
frang0doy <114451117+frang0doy@users.noreply.github.com>
2023-11-15 16:23:33 -0300

    primer commit

---
+++ src/main.js
@@ -0,0 +1,58 @@
+const productos = [
+   {
+      nombre: "Producto 1",
+      imagen: "producto1.jpg",
+      descripcion: "Descripción del Producto 1.",
+   },
+   {
+      nombre: "Producto 2",
+      imagen: "producto2.jpg",
+      descripcion: "Descripción del Producto 2.",
+   },
+   {
+      nombre: "Producto 3",
+      imagen: "producto3.jpg",
+      descripcion: "Descripción del Producto 3.",
+   },
+   {
+      nombre: "Producto 4",
+      imagen: "producto4.jpg",
+      descripcion: "Descripción del Producto 4.",
+   },
+   {
+      nombre: "Producto 5",
+      imagen: "producto5.jpg",
+      descripcion: "Descripción del Producto 5.",
+   },
+];
+
+// Obtén la referencia al elemento <section> de productos
+const productosSection = document.getElementById("productos-nuevos");
+
+// Recorre el array de productos y crea una tarjeta para cada uno
+productos.forEach((producto) => {
+   // Crea un elemento <article> para cada producto
+   const productoCard = document.createElement("article");
+   productoCard.classList.add("producto-card");
+
+   // Crea una imagen
+   const imagen = document.createElement("img");
+   imagen.src = producto.imagen;
+   imagen.alt = producto.nombre;
+
+   // Crea un título
+   const titulo = document.createElement("h3");
+   titulo.textContent = producto.nombre;
+
+   // Crea una descripción
+   const descripcion = document.createElement("p");
+   descripcion.textContent = producto.descripcion;
+
+   // Agrega los elementos al <article> del producto
+   productoCard.appendChild(imagen);
+   productoCard.appendChild(titulo);
+   productoCard.appendChild(descripcion);
+
+   // Agrega la tarjeta de producto al <section> de productos
+   productosSection.appendChild(productoCard);
+});
